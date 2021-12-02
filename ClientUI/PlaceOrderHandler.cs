675a791d657c422d58d3513be684e7325b363111
jdw <johndward01@gmail.com>
2021-12-01 21:39:03 -0600

    stopping point: https://docs.particular.net/tutorials/nservicebus-step-by-step/2-sending-a-command/

---
+++ ClientUI/PlaceOrderHandler.cs
@@ -0,0 +1,54 @@
+﻿using Messages.Commands;
+using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUI
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+            return Task.CompletedTask;
+        }
+
+        static async Task RunLoop(IEndpointInstance endpointInstance)
+        {
+            while (true)
+            {
+                log.Info("Press 'P' to place an order, or 'Q' to quit.");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        // Instantiate the command
+                        var command = new PlaceOrder
+                        {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+
+                        // Send the command to the local endpoint
+                        log.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+                        await endpointInstance.SendLocal(command).ConfigureAwait(false);
+                        break;
+
+                    case ConsoleKey.Q:
+                        return;
+
+                    default:
+                        log.Info("Unknown input. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
